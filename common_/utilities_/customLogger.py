c335f6f14f5ad783ad9530d0044d68ccdfad1864
Syuzanna <Syuzi.demirtshyan@gmail.com>
2023-12-11 16:13:34 +0400

    QAG2-120: did some minor fi~x

--- common_/utilities_/customLogger.py
+++ common_/utilities_/customLogger.py
@@ -14,6 +14,7 @@ def get_root_dir_name():
             command += ".parent"
 
 
+
 def logger(level, message, fileName=os.path.join(get_root_dir_name(), '_logs_', f'log_{date.today()}.log')):
     logging.basicConfig(level=logging.INFO, filename=fileName, filemode="a",
                         format="%(asctime)-12s %(levelname)s %(message)s",
